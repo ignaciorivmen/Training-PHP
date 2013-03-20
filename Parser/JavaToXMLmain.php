3d79c5a9e9ff57f460fe5dc4ba4352118ebd524b
Ignacio <enanorivas@enanorivas-Aspire-M3610.(none)>
2013-03-20 14:48:48 +0100

    Formularios -> Forms

--- Parser/JavaToXMLmain.php
+++ Parser/JavaToXMLmain.php
@@ -3,9 +3,9 @@
 	include 'ToBeanXML.php';
 	
 	$n = 2;	//numero de Beans a crear.
-	$dir = "javas/Announcement.java";
-	$nombreAtri = "announcement";
-	$nombre = "domain.Announcement";
+	$dir = "javas/Question.java";
+	$nombreAtri = "question";
+	$nombre = "domain.Question";
 	$vars = parsePrivateAttribute($dir);
 	ToBeanXML($dir, $vars, $n, $nombreAtri, $nombre);
 			
